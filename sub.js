285f5b67c47bb0bf435a3a2b5576ff3658f4d33b
Rebecca <reb343@cornell.edu>
2016-12-22 10:58:31 -0500

    added code to deal with srcset, haven't figured out page reload issue (if it's even reload that's the issue)

--- sub.js
+++ sub.js
@@ -12,6 +12,7 @@ var boList = ["Bo_1", "Bo_2", "Bo_3", "Bo_4"];
 function findTrumps()
 {
   var images = document.getElementsByTagName('img');
+  console.log("trumping");
   for (var i = 0, l = images.length; i < l; i++)
   {
     //TODO determine if pic is of Trump
@@ -22,10 +23,14 @@ function findTrumps()
 
     // check data-mediaviewer-caption for Trump
 
+    // TODO stop replacing all images
     if (true)
     {
       replace(images[i]);
     }
+
+  // TODO deal with links with background images?
+
   }
 }
 
@@ -34,9 +39,18 @@ function replace(image)
 {
   // choose random Bo
   var chosenBo = boList[Math.floor(Math.random() * boList.length)];
+  var newrl = chrome.extension.getURL("/images/" + chosenBo + ".jpg");
   // switch img src to be of Bo (should works b/c http://stackoverflow.com/questions/518000/is-javascript-a-pass-by-reference-or-pass-by-value-language)
   //image.src = "chrome-extension://__MSG_@@extension_id__/images/" + chosenBo + ".jpg";
-  image.src = chrome.extension.getURL("/images/" + chosenBo + ".jpg");
+  image.src = newrl;
+
+  // srcset: http://i1.wp.com/cornellsun.com/wp-content/uploads/2016/11/Pg-10-Bruce-Monger-by-David-Navadeh-File-Photo.jpg?w=1000 1000w, http://i1.wp.com/cornellsun.com/wp-content/uploads/2016/11/Pg-10-Bruce-Monger-by-David-Navadeh-File-Photo.jpg?resize=336%2C240 336w, http://i1.wp.com/cornellsun.com/wp-content/uploads/2016/11/Pg-10-Bruce-Monger-by-David-Navadeh-File-Photo.jpg?resize=768%2C548 768w, http://i1.wp.com/cornellsun.com/wp-content/uploads/2016/11/Pg-10-Bruce-Monger-by-David-Navadeh-File-Photo.jpg?resize=771%2C550 771w
+
+  // deal with srcset
+  // url regex (from : http://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url)
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var urlRegex = new RegExp(expression);
+  image.srcset = image.srcset.replace(urlRegex, newrl);
 
   // TODO limit size
   image.maxwidth = image.width;
@@ -44,9 +58,22 @@ function replace(image)
   image.maxheight = image.height;
   image.height = image.height;
 
-  console.log("width: "+image.width);
-  console.log("height: "+image.height);
 
 }
 
+function run()
+{
+  var a = false;
+  while(a)
+  {
+    if(performance.navigation.type  == 1 )
+    {
+      console.log('page reloaded');
+      a = false;
+    }
+  }
+}
+
 findTrumps();
+window.addEventListener('load', findTrumps, false);
+run();
